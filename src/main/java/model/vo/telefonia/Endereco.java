fc1b2ca186ab4094cbbf1155701620e2f4ca1195
leonardo.coutinho <leonardo.coutinho@FLE207D27.mshome.net>
2023-03-09 11:11:59 -0300

    Exercicio da telefonia
    - criação das entidades
    -criação dos scripts do banco

---
+++ src/main/java/model/vo/telefonia/Endereco.java
@@ -0,0 +1,105 @@
+package model.vo.telefonia;
+
+public class Endereco {
+
+	// ATRIBUTOS
+	private Integer id;
+	private String cep;
+	private String rua;
+	private String numero;
+	private String bairro;
+	private String cidade;
+	private String estado;
+
+	// CONSTRUTOR
+
+	public Endereco() {
+
+	}
+	
+
+	public Endereco(Integer id, String cep, String rua, String numero, String bairro, String cidade, String estado) {
+		super();
+		this.id = id;
+		this.cep = cep;
+		this.rua = rua;
+		this.numero = numero;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.estado = estado;
+	}
+
+
+	public Endereco(String cep, String rua, String numero, String bairro, String cidade, String estado) {
+		super();
+		this.cep = cep;
+		this.rua = rua;
+		this.numero = numero;
+		this.bairro = bairro;
+		this.cidade = cidade;
+		this.estado = estado;
+	}
+
+	// METODOS GETTER AND SETTERS
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getRua() {
+		return rua;
+	}
+
+	public void setRua(String rua) {
+		this.rua = rua;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String toString() {
+		return "Endereco [cep=" + cep + ", rua=" + rua + ", numero=" + numero + ", bairro=" + bairro + ", cidade="
+				+ cidade + ", estado=" + estado + "]";
+	}
+}
